f82f0bf7b1cb4621ebcdec6f76ec9a13fc940b55
yaya75315 <55869001+yaya75315@users.noreply.github.com>
2020-01-21 13:30:55 +0800

    web

---
+++ 20181110/js/jquery.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $(".title").click(function () {
+        $(".text").slideToggle("1000");
+    });
+});
+
+$(document).ready(function () {
+    $(".taititle").click(function () {
+        $(".taitext").slideToggle("1000");
+    });
+});
+
+$(document).ready(function () {
+    $(".taititle01").click(function () {
+        $(".taitext01").slideToggle("1000");
+    });
+});
